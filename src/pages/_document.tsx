a7f193b9adafa4dc9b13a63b41052bc606b7cd44
Santosl2c <mfilype2019@gmail.com>
2022-05-18 23:57:39 -0300

    feat: PWA config finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,21 @@ export default class MyDocumment extends Document {
     return (
       <Html>
         <Head>
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
