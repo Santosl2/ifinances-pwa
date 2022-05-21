31f7674282dbcaafb6b46d79ec21757c6cb38283
Santosl2c <mfilype2019@gmail.com>
2022-05-21 17:15:55 -0300

    fix: responsive design fix

--- src/components/Summary/Summary.styles.ts
+++ src/components/Summary/Summary.styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const SummaryWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 3rem;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 1.5rem;
   margin-top: -9rem;
 
   div:last-child {
