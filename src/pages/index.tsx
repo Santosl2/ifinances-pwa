7ef55e4a64c062ad4dc325cee04bb860b13551ca
Santosl2c <mfilype2019@gmail.com>
2022-05-28 11:29:44 -0300

    <feat>: animated login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,39 @@
 import { Button, Input } from "@/components/Form";
+import { SEO } from "@/SEO";
 import { LoginContent, LoginWrapper } from "@styles/Login.styles";
+
+const LoginVariants = {
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+  hidden: {
+    opacity: 0,
+    x: "-50%",
+  },
+};
 
 export default function Login() {
   return (
-    <LoginWrapper>
-      <h4>iFinances</h4>
-      <LoginContent>
-        <Input name="email" type="email" label="E-mail" />
-        <Input name="password" type="password" label="Senha" />
-        <Button type="submit" bgColor="#36314f">
-          Entrar
-        </Button>
+    <>
+      <SEO title="Controle suas finanças de maneira fácil" />
+      <LoginWrapper
+        initial="hidden"
+        animate="visible"
+        variants={LoginVariants}
+        transition={{ duration: 0.7 }}
+      >
+        <h4>💸 iFinances</h4>
+        <LoginContent>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+          <Button type="submit" bgColor="#36314f">
+            Entrar
+          </Button>
 
-        <Button type="button">Registre-se</Button>
-      </LoginContent>
-    </LoginWrapper>
+          <Button type="button">Registre-se</Button>
+        </LoginContent>
+      </LoginWrapper>
+    </>
   );
 }
--- src/styles/Login.styles.ts
+++ src/styles/Login.styles.ts
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const LoginWrapper = styled.main`
+export const LoginWrapper = styled(motion.main)`
   width: 100vw;
   height: 100vh;
   display: flex;
