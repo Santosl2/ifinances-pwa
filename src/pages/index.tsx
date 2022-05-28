005dd02dea46123476b52734350cac224fa7d21b
Santosl2c <mfilype2019@gmail.com>
2022-05-28 11:20:10 -0300

    <chore>: minor adjusts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,15 @@ import { LoginContent, LoginWrapper } from "@styles/Login.styles";
 export default function Login() {
   return (
     <LoginWrapper>
+      <h4>iFinances</h4>
       <LoginContent>
         <Input name="email" type="email" label="E-mail" />
         <Input name="password" type="password" label="Senha" />
+        <Button type="submit" bgColor="#36314f">
+          Entrar
+        </Button>
 
-        <Button type="submit">Send</Button>
+        <Button type="button">Registre-se</Button>
       </LoginContent>
     </LoginWrapper>
   );
