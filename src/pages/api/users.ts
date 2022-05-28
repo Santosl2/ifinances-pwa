ea68690e5977a1c76ec192d7388ffde7eb8d6ec5
Santosl2c <mfilype2019@gmail.com>
2022-05-28 11:30:27 -0300

    <chore>: minor adjusts

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,12 +1,12 @@
-import { addDoc, collection } from "firebase/firestore";
 import { NextApiRequest, NextApiResponse } from "next";
-
-import { database } from "@/services/firebase";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // to do create user
-  const dbInstance = collection(database, "users");
-  addDoc(dbInstance, {
-    title: "opa",
-  });
+  if (req.method === "POST") {
+    console.log(req);
+  }
+  // const dbInstance = collection(database, "users");
+  // addDoc(dbInstance, {
+  //   title: "opa",
+  // });
 };
