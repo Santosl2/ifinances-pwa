b1c6e1b62f4961d352619e10f2044ce8753d5d7b
Santosl2c <mfilype2019@gmail.com>
2022-05-29 15:06:14 -0300

    <feat>: register page finished

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,12 +1,51 @@
+/* eslint-disable camelcase */
+/* eslint-disable unused-imports/no-unused-vars */
+import { hash } from "bcryptjs";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { NextApiRequest, NextApiResponse } from "next";
+
+import { SignUpFormData } from "@/interfaces/Forms";
+import { database } from "@/services/firebase";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // to do create user
   if (req.method === "POST") {
-    console.log(req);
+    const { name, email, password } = req.body?.user as SignUpFormData;
+
+    const auth = getAuth();
+    const dbInstance = collection(database, "users");
+
+    const q = query(dbInstance, where("email", "==", email));
+    const queryResult = await getDocs(q);
+
+    if (queryResult.size > 0) {
+      return res.json({ success: false, message: "E-mail já cadastrado." });
+    }
+
+    try {
+      const createUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const hashedPassword = await hash(password, 8);
+
+      addDoc(dbInstance, {
+        id: createUser.user.uid,
+        name,
+        email,
+        password: hashedPassword,
+        authProvider: "local",
+      });
+
+      return res.json({ success: true });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: "Ocorreu um erro ao cadastrar.",
+      });
+    }
   }
-  // const dbInstance = collection(database, "users");
-  // addDoc(dbInstance, {
-  //   title: "opa",
-  // });
 };
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { toast } from "react-toastify";
+
 import { useRouter } from "next/router";
+import * as yup from "yup";
 
 import { Button, Input } from "@/components/Form";
+import { SignUpFormData } from "@/interfaces/Forms";
 import { SEO } from "@/SEO";
+import { api } from "@/services/api";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { LoginContent, LoginWrapper } from "@styles/Login.styles";
 
 const LoginVariants = {
@@ -15,8 +24,50 @@ const LoginVariants = {
   },
 };
 
+const registerSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().email("E-mail inválido").required("E-mail obrigatório"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "No mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "Senhas não conferem")
+    .required("Senha obrigatória"),
+});
+
 export default function Register() {
   const router = useRouter();
+
+  const createUser = useMutation(async (user: SignUpFormData) => {
+    const response = await api.post("/users", {
+      user,
+    });
+
+    return response.data;
+  });
+
+  const { register, handleSubmit, formState, reset } = useForm<SignUpFormData>({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const handleRegister: SubmitHandler<SignUpFormData> = async (data) => {
+    const response = await createUser.mutateAsync(data);
+
+    toast(response.message ?? "Seja bem-vindo(a)", {
+      type: `${response.success ? "success" : "error"}`,
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+    reset();
+  };
 
   return (
     <>
@@ -28,21 +79,45 @@ export default function Register() {
         transition={{ duration: 0.7 }}
       >
         <h4>💸 Registro</h4>
-        <LoginContent>
-          <Input name="name" type="text" label="Nome" required />
-
-          <Input name="email" type="email" label="E-mail" required />
-
-          <Input name="password" type="password" label="Senha" required />
-
+        <LoginContent onSubmit={handleSubmit(handleRegister)}>
           <Input
-            name="repeatPassword"
-            type="password"
-            label="Repetir senha"
+            type="text"
+            label="Nome"
+            error={formState.errors.name}
+            {...register("name")}
             required
           />
 
-          <Button type="submit" bgColor="#36314f">
+          <Input
+            type="email"
+            label="E-mail"
+            error={formState.errors.email}
+            {...register("email")}
+            required
+          />
+
+          <Input
+            type="password"
+            label="Senha"
+            error={formState.errors.password}
+            {...register("password")}
+            required
+          />
+
+          <Input
+            type="password"
+            label="Repetir senha"
+            error={formState.errors.password_confirmation}
+            {...register("password_confirmation")}
+            required
+          />
+
+          <Button
+            type="submit"
+            bgColor="#36314f"
+            isLoading={formState.isSubmitting}
+            disabled={formState.isSubmitting}
+          >
             Registrar
           </Button>
 
