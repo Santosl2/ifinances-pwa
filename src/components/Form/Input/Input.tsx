ddc3458651eac710bd20e523b44a591496580d19
Santosl2c <mfilype2019@gmail.com>
2022-05-29 14:20:45 -0300

    <feat>: input error

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,12 +1,27 @@
+/* eslint-disable react/function-component-definition */
 /* eslint-disable react/jsx-props-no-spreading */
+import { forwardRef, ForwardRefRenderFunction } from "react";
+
 import { InputStyled, InputWrapper } from "./Input.styles";
 import { InputProps } from "./Input.types";
 
-export function Input({ label, name, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { label, name, error, ...rest },
+  ref
+) => {
   return (
-    <InputWrapper>
+    <InputWrapper error={error}>
       {!!label && <label htmlFor={name}>{label}</label>}
-      <InputStyled name={name} id={name} placeholder={label} {...rest} />
+      <InputStyled
+        name={name}
+        id={name}
+        placeholder={label}
+        ref={ref}
+        {...rest}
+      />
+      {!!error && <span>{error.message}</span>}
     </InputWrapper>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
