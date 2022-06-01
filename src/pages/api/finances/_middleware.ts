0ca4015047678022dbc7e7606b8880d9dc30baf2
Santosl2c <mfilype2019@gmail.com>
2022-06-01 00:59:11 -0300

    feat: protected API routes

---
+++ src/pages/api/finances/_middleware.ts
@@ -0,0 +1,13 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { headers } = req;
+  const authHeader = headers.get("Authorization");
+
+  if (authHeader) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.json("Unauthorized");
+}
