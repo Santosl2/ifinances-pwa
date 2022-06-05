5f4e638b9d166eaddd3edc486eac7d4eaaf5da00
Santosl2c <mfilype2019@gmail.com>
2022-06-04 22:41:06 -0300

    feat: Open modal when click em New Transaction button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,23 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+
 import { Button } from "@/components/Form";
+import { closeModal } from "@/store/modal/ModalReducers";
 
 import { HeaderContent, HeaderWrapper } from "./Header.styles";
 
 export function Header(): JSX.Element {
+  const dispatch = useDispatch();
+
+  const handleOpenModal = useCallback(
+    () => dispatch(closeModal({ isOpen: true })),
+    []
+  );
   return (
     <HeaderWrapper>
       <HeaderContent>
         <img src="/iFinances.svg" alt="iFinances Logo" />
-        <Button>Nova transação</Button>
+        <Button onClick={handleOpenModal}>Nova transação</Button>
       </HeaderContent>
     </HeaderWrapper>
   );
