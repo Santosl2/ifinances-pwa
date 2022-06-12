859117d704a0fa5625a714b6249d9dd021961602
Santosl2c <mfilype2019@gmail.com>
2022-06-12 15:19:42 -0300

    feat: user create finance created

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,4 +1,5 @@
-import { UserFinancesResponse } from "@/interfaces/Response";
+import { CreateTransactionModalFormData } from "@/interfaces/Forms";
+import { DefaultResponse, UserFinancesResponse } from "@/interfaces/Response";
 
 import { api } from "../api";
 
@@ -6,4 +7,14 @@ export async function getUsersFinances(): Promise<UserFinancesResponse[]> {
   const { data } = await api.get("/finances");
 
   return data;
+}
+
+export async function createUserFinances(
+  data: CreateTransactionModalFormData
+): Promise<DefaultResponse> {
+  const response = await api.post("/finances", {
+    data,
+  });
+
+  return response.data;
 }
