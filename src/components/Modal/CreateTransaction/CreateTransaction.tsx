580cdd18c1e7a57f7fa34b29ad384f54d68095db
Santosl2c <mfilype2019@gmail.com>
2022-06-12 14:30:31 -0300

    feat: Create Transaction finished

--- src/components/Modal/CreateTransaction/CreateTransaction.tsx
+++ src/components/Modal/CreateTransaction/CreateTransaction.tsx
@@ -1,10 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useCallback, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import ReactModal from "react-modal";
+import { useMutation } from "react-query";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
+import * as yup from "yup";
 
 import { Button } from "@/components/Form";
 import { useSelectorModal } from "@/hooks/useSelectorModal";
+import {
+  CreateTransactionModalFormData,
+  TransactionTypes,
+} from "@/interfaces/Forms";
+import { api } from "@/services/api";
+import { queryClient } from "@/services/queryClient";
 import { closeModal } from "@/store/modal/ModalReducers";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import {
   ModalForm,
@@ -15,12 +28,60 @@ import {
 
 ReactModal.setAppElement("body");
 
+const modalSchema = yup.object().shape({
+  transactionName: yup.string().required("Campo obrigatório"),
+  price: yup.number().required("Campo obrigatório"),
+  category: yup.string().required("Campo obrigatório"),
+});
+
 export function CreateTransactionModal() {
   const modal = useSelectorModal();
   const dispatch = useDispatch();
-  const [type, setType] = useState("deposit");
+  const [type, setType] = useState<TransactionTypes>("income");
+
+  const createTransaction = useMutation(
+    async (data: CreateTransactionModalFormData) => {
+      const response = await api.post("/finances", {
+        data,
+      });
+
+      return response.data;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("finances");
+      },
+    }
+  );
 
   const handleCloseModal = useCallback(() => dispatch(closeModal()), []);
+
+  const { register, handleSubmit, formState, reset } =
+    useForm<CreateTransactionModalFormData>({
+      resolver: yupResolver(modalSchema),
+    });
+
+  const handleTransactionCreate: SubmitHandler<
+    CreateTransactionModalFormData
+  > = async (data) => {
+    const response = await createTransaction.mutateAsync({
+      ...data,
+      type,
+    });
+
+    toast(response.message ?? "Transação cadastrada com sucesso!", {
+      type: `${response.success ? "success" : "error"}`,
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+    reset();
+  };
 
   return (
     <ReactModal
@@ -38,20 +99,20 @@ export function CreateTransactionModal() {
           <img src="close.svg" alt="Fechar modal" />
         </button>
 
-        <ModalForm>
+        <ModalForm onSubmit={handleSubmit(handleTransactionCreate)}>
           <h2>Cadastrar transação</h2>
 
-          <input placeholder="Nome" />
-          <input placeholder="Preço" />
-          <input placeholder="Categoria" />
+          <input placeholder="Nome" {...register("transactionName")} />
+          <input placeholder="Preço" {...register("price")} />
+          <input placeholder="Categoria" {...register("category")} />
 
           <TransactionTypeContainer>
             <RadioBox
               type="button"
               onClick={() => {
-                setType("deposit");
+                setType("income");
               }}
-              isActive={type === "deposit"}
+              isActive={type === "income"}
               activeColor="green"
             >
               <img src="income.svg" alt="Entrada" />
@@ -61,9 +122,9 @@ export function CreateTransactionModal() {
             <RadioBox
               type="button"
               onClick={() => {
-                setType("withdraw");
+                setType("outcome");
               }}
-              isActive={type === "withdraw"}
+              isActive={type === "outcome"}
               activeColor="red"
             >
               <img src="outcome.svg" alt="Saída" />
@@ -71,7 +132,9 @@ export function CreateTransactionModal() {
             </RadioBox>
           </TransactionTypeContainer>
 
-          <Button bgColor="#33CC95">Cadastrar</Button>
+          <Button bgColor="#33CC95" isLoading={formState.isSubmitting}>
+            Cadastrar
+          </Button>
         </ModalForm>
       </ModalWrapper>
     </ReactModal>
