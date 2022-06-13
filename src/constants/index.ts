9cf8553b1803af34c0979c950f18c7b9d4cf22a7
Santosl2c <mfilype2019@gmail.com>
2022-06-13 18:51:35 -0300

    feat: CSV button to export all table registers

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const LOGIN_COOKIE_NAME = "@ifinances/user";
 export const LOGIN_COOKIE_REFRESH_TOKEN = "@ifinances/refresh-token";
 export const LOGIN_COOKIE_ACCESS_TOKEN = "@ifinances/access-token";
+export const CSV_EXPORT_FILE_NAME = "ifinances-extrato";
